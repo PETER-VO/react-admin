b03cecc938e69aaaf36cb82a4897dea8bc0ef304
Cudanglun13759350 <v.ducphong@gmail.com>
2021-09-05 14:49:27 +0300

    adding homepage and featuredinfo component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './sidebar.css';
-import {LineStyle, Timeline, TrendingUp} from '@material-ui/icons'
+import {BarChart, ChatBubbleOutline, DynamicFeed, LineStyle, MailOutline, PermIdentity, Report, Storefront, Timeline, TrendingUp, WorkOutline} from '@material-ui/icons'
 
 export default function Sidebar() {
 	return(
@@ -20,6 +20,61 @@ export default function Sidebar() {
 						<li className='sidebarListItem'>
 							<TrendingUp className='sidebarIcon'/>
 							Sales
+						</li>
+					</ul>
+				</div>
+				<div className='sidebarMenu'>
+					<h3 className='sidebarTitle'>Quick Menu</h3>
+					<ul className='sidebarList'>
+						<li className='sidebarListItem active'>
+							<PermIdentity className='sidebarIcon'/>
+							Users
+						</li>
+						<li className='sidebarListItem'>
+							<Storefront className='sidebarIcon'/>
+							Products
+						</li>
+						<li className='sidebarListItem'>
+							<TrendingUp className='sidebarIcon'/>
+							Transactions
+						</li>
+						<li className='sidebarListItem'>
+							<BarChart className='sidebarIcon'/>
+							Reports
+						</li>
+					</ul>
+				</div>
+				<div className='sidebarMenu'>
+					<h3 className='sidebarTitle'>Notifications</h3>
+					<ul className='sidebarList'>
+						<li className='sidebarListItem active'>
+							<MailOutline className='sidebarIcon'/>
+							Mail
+						</li>
+						<li className='sidebarListItem'>
+							<DynamicFeed className='sidebarIcon'/>
+							Feedback
+						</li>
+						<li className='sidebarListItem'>
+							<ChatBubbleOutline className='sidebarIcon'/>
+							Messages
+						</li>
+					</ul>
+				</div>
+				<div className='sidebarMenu'>
+					<h3 className='sidebarTitle'>Staff</h3>
+					<ul className='sidebarList'>
+						<li className='sidebarListItem active'>
+							<WorkOutline className='sidebarIcon'/>
+							Manage
+						</li>
+						<li className='sidebarListItem'>
+							<Timeline className='sidebarIcon'/>
+							Analytics
+						</li>
+						<li className='sidebarListItem'>
+							<Report className='sidebarIcon'/>
+							Reports
 						</li>
 					</ul>
 				</div>
